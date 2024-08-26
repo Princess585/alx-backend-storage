d03e2678a1e95051a3a70309fc62426bbd637c97
Princess585 <princessdkhombisa1994@gmail.com>
2024-08-26 08:25:16 +0000

    updated

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,3 +1,3 @@
 -- SQL script that creates a trigger that decreases qty
 CREATE TRIGGER dec_q AFTER INSERT ON orders FOR EACH ROW
-UPDATE items SET quantity = quantity - NEW.quantity WHERE name=NEW.item_name;
+UPDATE items SET quantity = quantity - NEW.number WHERE name=NEW.item_name;
--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,21 +1,15 @@
 -- SQL script that creates a stored procedure AddBonus
-DELIMETER
-CREATE OR REPLACE PROCEDURE AddBonus(
-    IN user_id INT,
-    IN project_name VARCHAR(255),
-    IN score INT
-)
-DECLARE
-    project_id INT;
+DELIMITER //
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
+    DECLARE project_id INT;
+
     SELECT id INTO project_id FROM projects WHERE name = project_name;
-    
     IF project_id IS NULL THEN
-        INSERT INTO projects (name) VALUES (project_name) RETURNING id INTO project_id;
+        INSERT INTO projects (name) VALUES (project_name);
+        SET project_id = LAST_INSERT_ID();
     END IF;
 
-    INSERT INTO corrections (user_id, project_id, score) 
-    VALUES (user_id, project_id, score);
-
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
 END;//
-DELIMETER ;
+DELIMITER ;
